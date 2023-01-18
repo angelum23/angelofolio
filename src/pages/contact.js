790ea0e09e60b2153d6455687a460457ed23d0f7
angelum23 <angelojosedarosa01@hotmail.com>
2023-01-18 01:39:38 -0300

    Organizado app.css e adicionado envio de email (incompleto) na aba contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { TextField, Button } from '@mui/material';
   
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 
+                     'YOUR_TEMPLATE_ID',
+                     form.current,
+                     'YOUR_PUBLIC_KEY').then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+  };
+
   return (
-    <div>
-      <h1>Contact me on angelojosedarosa01@gmail.com</h1>
+    <div class='divcontact'>
+      <form ref={form} onSubmit={sendEmail} class="formcontact">
+
+        <h2>Want to contact?<br/>Email me direct from here</h2>
+        <label for="user_name">Full name</label><br/>
+        <TextField id="user_name" name="user_name" variant="outlined" /> 
+        
+        <label for="user_email">Email</label><br/>
+        <TextField id="user_email" name="user_email" variant="outlined" />
+        
+        <label for="message">Message</label><br/>
+        <TextField id="message" name="message" variant="outlined" />
+        <br/><br/>
+        <Button variant="contained" type="submit" value="Send">Send</Button>
+      </form>
     </div>
   );
 };
