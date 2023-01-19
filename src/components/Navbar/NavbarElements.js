4ea96a1b240e931b8119e1101080a0f72904431f
angelum23 <angelojosedarosa01@hotmail.com>
2023-01-19 16:30:39 -0300

    finalizada tela de contatos

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #141318;
+  background: #000000;
   height: 85px;
   display: flex;
   justify-content: center;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,11 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { TextField, Button } from '@mui/material';
-  
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+
 const Contact = () => {
   const form = useRef();
 
@@ -19,21 +23,42 @@ const Contact = () => {
   };
 
   return (
-    <div class='divcontact'>
-      <form ref={form} onSubmit={sendEmail} class="formcontact">
+    <div>
+      <NotificationContainer/>
 
-        <h2>Want to contact?<br/>Email me direct from here</h2>
-        <label for="user_name">Full name</label><br/>
-        <TextField id="user_name" name="user_name" variant="outlined" /> 
-        
-        <label for="user_email">Email</label><br/>
-        <TextField id="user_email" name="user_email" variant="outlined" />
-        
-        <label for="message">Message</label><br/>
-        <TextField id="message" name="message" variant="outlined" />
-        <br/><br/>
-        <Button variant="contained" type="submit" value="Send">Send</Button>
-      </form>
+      <div class='divemailcontact'>
+        <form ref={form} onSubmit={sendEmail}>
+
+          <h2>Want to contact? Email me direct from here</h2>
+
+          <label for="user_name">Full name</label><br/>
+          <TextField id="user_name" name="user_name" variant="outlined" multiline style = {{width: 350}}/> <br/><br/>
+          
+          <label for="user_email">Email</label><br/>
+          <TextField id="user_email" name="user_email" variant="outlined" style = {{width: 350}}/> <br/><br/>
+          
+          <label for="message">Message</label><br/>
+          <TextField id="message" name="message" variant="outlined" fullWidth multiline rows={6} maxRows={20} placeholder="Email sendind still not working but it will be implemented soon."/> <br/><br/>
+
+          <Button variant="contained" type="submit" value="Send" color="secondary">Send</Button>
+        </form>
+      </div>
+
+      <div class='divdatacontact'>
+        <h2 class="contacts">Contact links</h2>
+        <h3 class="texto">
+          <p><FaGithub/> <a href="https://github.com/angelum23">GitHub</a></p>
+          <p><FaLinkedin/> <a href="https://www.linkedin.com/in/ângelo-josé-da-rosa-7b1bb11b9/">Linkedin</a></p>
+
+          <CopyToClipboard text={"angelojose.net@gmail.com"}
+            onCopy={() => {
+              NotificationManager.info('Email successfully copied to clipboard!');
+            }}>
+            <Button variant='contained' color='secondary'>Email</Button>
+          </CopyToClipboard>
+
+        </h3>
+      </div>
     </div>
   );
 };
